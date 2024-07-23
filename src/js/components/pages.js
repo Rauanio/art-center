7fab48c2c772bc8c6596a78309be93c72253bc3a
Rauanio <rauanovich01@mail.ru>
2024-07-24 01:10:49 +0500

    add advertising, tenant pages

--- src/js/components/pages.js
+++ src/js/components/pages.js
@@ -1,3 +1,13 @@
+const pages = [
+  'purchases',
+  'parents',
+  'activity',
+  'business',
+  'sport',
+  'services',
+  'guests',
+];
+
 document.addEventListener('DOMContentLoaded', function () {
   const body = document.body;
   const currentPage = body.getAttribute('class');
@@ -5,20 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
   if (!currentPage) {
     const path = window.location.pathname;
 
-    if (path.includes('purchases')) {
-      body.classList.add('purchases');
-    } else if (path.includes('parents')) {
-      body.classList.add('parents');
-    } else if (path.includes('activity')) {
-      body.classList.add('activity');
-    } else if (path.includes('business')) {
-      body.classList.add('business');
-    } else if (path.includes('sport')) {
-      body.classList.add('sport');
-    } else if (path.includes('services')) {
-      body.classList.add('services');
-    } else if (path.includes('guests')) {
-      body.classList.add('guests');
-    }
+    pages.map((page) => {
+      if (path.includes(page)) {
+        body.classList.add(page);
+      }
+    });
   }
 });
