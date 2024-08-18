31160f539ac22cd6ed46d406d029cfaccfc674ca
Rauanio <rauanovich01@mail.ru>
2024-08-19 01:31:08 +0500

    fix duration

--- src/js/components/preLoader.js
+++ src/js/components/preLoader.js
@@ -1,14 +1,16 @@
-window.addEventListener('load', function () {
+document.addEventListener('DOMContentLoaded', function () {
   const preloader = document.querySelector('.pre__loader');
 
-  const loaderDuration = 1500;
-
-  setTimeout(() => {
-    preloader.style.transition = 'opacity 0.3s ease';
-    preloader.style.opacity = '0';
+  window.addEventListener('load', function () {
+    const loaderDuration = 1000;
 
     setTimeout(() => {
-      preloader.style.display = 'none';
-    }, 500);
-  }, loaderDuration);
+      preloader.style.transition = 'opacity 0.5s ease';
+      preloader.style.opacity = '0';
+
+      setTimeout(() => {
+        preloader.style.display = 'none';
+      }, 500);
+    }, loaderDuration);
+  });
 });
